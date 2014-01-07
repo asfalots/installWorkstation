5504414010287e494b4c95c1594e0b7ed923888e
Erwan Grooters <erwan.grooters@alphanetworks.be>
2014-01-07 15:42:37 +0100

    Add Menu choice possibility instead of only automatic install

--- install.sh
+++ install.sh
@@ -9,25 +9,6 @@ if [ "$(whoami)" != 'root' ]; then
 fi
 
 
-exec(){
-	echo -en "$1...\r"
-
-	$2 2>/tmp/error.log >/dev/null
-
-
-	if(($? == O)); then
-
-		echo -e "\033[32m [OK] \033[0m $1"
-	else
-		echo -e "\033[33m [FAILED] \033[0m $1"
-		cat /tmp/error.log
-	fi
-
-}
-
-PACKET_LIST="geany guake openjdk-7-jre zend-server-php-5.4 pgadmin3 git gitk putty filezilla nfs-common firefox chromium-browser terminator playonlinux mysql-workbench oracle-java7-installer ssh shutter meld"
-
-
 #Detect System
 if [ $(getconf LONG_BIT) = '64' ]; then
 	ZEND_STUDIO_URL="http://downloads.zend.com/studio-eclipse/10.5.0/ZendStudio-10.5.0-linux.gtk.x86_64.tar.gz"
@@ -47,44 +28,72 @@ POSTGRES_REPO="deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main"
 COMPOSER_URL="http://getcomposer.org/composer.phar"
 
 
-echo $ZEND_SERVER_REPO > /etc/apt/sources.list.d/zend.list
-wget http://repos.zend.com/zend.key -O- 2> /dev/null | apt-key add - >/dev/null
-exec "Add Zend repository" " "
-
-
-#Ajout Depot Postgres
-
-echo $POSTGRES_REPO > /etc/apt/sources.list.d/postgres.list
-wget https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- 2> /dev/null | apt-key add - >/dev/null
-exec "Add Postgresql repository"
-
-#Ajout Depot Gnome PPA
-add-apt-repository -y ppa:gnome3-team/gnome3 > /dev/null 2>&1
-exec "Add Gnome3 repository"
-
-#Ajout depot Sun Java
-add-apt-repository -y ppa:webupd8team/java > /dev/null 2>&1
-exec "Add Sun Java repository"
+FUNCLIST="update_gnome3 install_basic install_zendserver install_postgres install_javasun install_git install_zendstudio install_sublime install_composer install_soapui install_vbox install_yed install_pgmodeler"
 
 
 
-exec "Update repositories" "apt-get update"
-exec "Update Packages" "apt-get dist-upgrade -y"
+exec(){
+	echo -en "$1...\r"
 
-for PACKET in $PACKET_LIST
-do
-	exec "Installing $PACKET" "apt-get install -y $PACKET"
-done
-
-#Config GIT
-git config --global mergetool.keepBackup false
-git config --global branch.autosetuprebase always
-git config --global pull.rebase true
-git config --global core.autocrlf input
-git config --global core.safecrlf true
+	$2 2>/tmp/error.log >/dev/null
 
 
-install_zs(){
+	if(($? == O)); then
+
+		echo -e "\033[32m [OK] \033[0m $1"
+	else
+		echo -e "\033[33m [FAILED] \033[0m $1"
+		cat /tmp/error.log
+	fi
+
+}
+
+
+install_zendserver(){
+	echo $ZEND_SERVER_REPO > /etc/apt/sources.list.d/zend.list
+	wget http://repos.zend.com/zend.key -O- 2> /dev/null | apt-key add - >/dev/null
+	apt-get update
+	apt-get install -y zend-server-php-5.4
+}
+
+install_postgres(){
+	echo $POSTGRES_REPO > /etc/apt/sources.list.d/postgres.list
+	wget https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- 2> /dev/null | apt-key add - >/dev/null
+	apt-get update
+	apt-get install -y postgresql-server-9.3 pgadmin3
+}
+
+install_javasun(){
+	add-apt-repository -y ppa:webupd8team/java
+	apt-get update
+	apt-get install -y oracle-java7-installer
+}
+
+update_gnome3(){
+	add-apt-repository -y ppa:gnome3-team/gnome3
+	apt-get update
+	apt-get dist-upgrade -y
+}
+
+install_git(){
+
+	apt-get install -y "git gitk"
+	git config --global mergetool.keepBackup false
+	git config --global branch.autosetuprebase always
+	git config --global pull.rebase true
+	git config --global core.autocrlf input
+	git config --global core.safecrlf true
+}
+
+install_basic(){
+	PACKET_LIST="geany guake putty filezilla nfs-common firefox chromium-browser terminator playonlinux ssh shutter meld"
+	for PACKET in $PACKET_LIST
+	do
+		exec "Installing $PACKET" "apt-get install -y $PACKET"
+	done
+}
+
+install_zendstudio(){
 	cd /tmp
 	wget -c $ZEND_STUDIO_URL >/dev/null
 	tar -zxvf Zend* >/dev/null
@@ -184,32 +193,45 @@ EOF
 
 }
 
-echo -en "Press enter to downlad Zend Studio or s for SKIP \r"
-read NEXT
-if($NEXT != 's');then
-	exec "Installing Zend Studio" "install_zs"
-fi
 
-echo -en "Press enter to install Composer or s for SKIP \r"
-read NEXT
-if($NEXT != 's');then
-	exec "Installing Composer" "install_composer"
-fi
 
-echo -en "Press enter to downlad Sublime Text 3 or s for SKIP \r"
-read NEXT
-if($NEXT != 's');then
-	exec "Installing Sublime Text 3" "install_sublime"
-fi
+fullInstall(){
+	for FUNC in $FUNCLIST
+	do
+		exec $(FUNC)
+	done
+}
 
-echo -en "Press enter to downlad yED or s for SKIP \r"
-read NEXT
-if($NEXT != 's');then
-	exec "Installing yED" "install_yed"
-fi
+menu(){
+	COUNTER=0
+	for FUNC in $FUNCLIST
+	do
+	    echo [$COUNTER] $FUNC
+	    COUNTER=$[COUNTER+1]
+	done
+	list=( $FUNCLIST)
 
-echo -en "Press enter to downlad Virtual Box or s for SKIP \r"
-read NEXT
-if($NEXT != 's');then
-	exec "" "install_vbox"
-fi
+	read CHOICE
+	clear
+	exec ${list[$CHOICE]} ${list[$CHOICE]}
+	menu
+}
+
+
+clear
+echo "Please choice what you want to do:"
+echo "[0] Full Install"
+echo "[1] Partial Install"
+read SELECT
+
+case $SELECT in
+	0)
+	clear
+	fullInstall
+	;;
+
+	1)
+	clear
+	menu
+	;;
+esac
